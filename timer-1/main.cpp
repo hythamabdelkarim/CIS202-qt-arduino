e025b4cc7c4d081424b1bb676036aaa32d85df91
hythamabdelkarim <31451997+hythamabdelkarim@users.noreply.github.com>
2018-05-29 16:22:56 -0700

    Update main.cpp

--- timer-1/main.cpp
+++ timer-1/main.cpp
@@ -1,3 +1,11 @@
+//
+// File: main.cpp
+// Description: This program will make a ball animation bounce in a random direction inside the widget rectangle.
+// Created: Tuesday. May 29, 2018, 4:00 pm
+// Author: Hytham Abdelkarim
+// mail: abdelkarimh@student.vvc.edu
+//
+
 #include "mainwindow.h"
 #include <QApplication>
 
